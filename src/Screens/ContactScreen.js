41e23f2cbf8ba9249f747a85a20f809900fc4f93
devrawan <rradwan2@students.iugaza.edu.ps>
2022-10-08 17:00:46 +0300

    header

--- src/Screens/ContactScreen.js
+++ src/Screens/ContactScreen.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {
+  ImageBackground,
     Image,
   Text,
   Pressable,
@@ -20,16 +21,16 @@ const ContactScreen  = () => {
     const {height, width} = useWindowDimensions();
 return(
     <View style={[styles.container,{ 
-        // backgroundColor:'black', 
         width:width,height:height}]}>
-          <Image  source={images.image}style={{width:width,height:"15%"}}/>
-          <Text style={{fontSize:22 ,color:'white',marginTop:-45,alignSelf:'center'}}>تواصل معنا   </Text>
+          <ImageBackground source={images.image} style={{width:width,height:140,justifyContent:"flex-end",alignItems:'center',paddingBottom:30}}>
+        <Text style={{fontSize:25 ,color:'white',alignSelf:'center',fontFamily:'Cairo-Bold'}}>تواصل معنا   </Text>
+        </ImageBackground>
           <View style={{paddingVertical:10,marginTop:40,width:"100%",lexDirection:'row',justifyContent:'center',alignItems:'center'}}>
             <Image source={images.ren} style={{width:220,height:220}}/>
           </View>
           <View style={{paddingVertical:15,marginTop:35,paddingHorizontal:20,width:'100%',flexDirection:'row',justifyContent:'flex-end'}}>
             <Text style={{fontSize:25,fontWeight:'500'}}> تواصل معنا علي : </Text>
-          </View>
+          </View> 
           <View style={{paddingVertical:5,marginTop:5,paddingHorizontal:20,width:'100%',flexDirection:'row',justifyContent:'flex-end'}}>
           <Text style={{fontSize:18,textDecorationLine:'underline'}}>infa@afaa8.com</Text>
           </View>
